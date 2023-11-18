779cfffad554ae29aa78514f0f61fef3edfd95c0
assimilate <abelladaniel1@gmail.com>
2023-11-18 23:00:47 +0100

    Fixed UI details. Doesn't work for Galaxy Fold.

--- src/ProductPage.tsx
+++ src/ProductPage.tsx
@@ -29,20 +29,24 @@ type ProductCardProps = {
 const ProductCard = ({ name, imageUri, price, id }: ProductCardProps) => {
   return (
     <div className="h-full w-full flex justify-center items-center flex-col bg-radial-gradient-second">
-      <div className="flex justify-center flex-col items-center bg-white space-y-4 w-72 p-4">
-        <h4 className="font-mono text-sm text-black">Billigaste priset</h4>
-        <h2 className="text-md text-red-500">{price} :-</h2>
-        <img className="w-32 h-32" src={imageUri} />
+      <div className="flex justify-center shadow-lg flex-col items-center bg-white w-72 p-4 sm:w-96 sm:h-[30rem]">
+        <h4 className="font-mono text-sm text-black mt-2 sm:mt-6">Billigaste priset</h4>
+        <h2 className="text-3xl text-red-500">{price} :-</h2>
+        <div className="flex items-center justify-center flex-col sm:mt-20">
+        <img className="w-28 h-28 mt-6 sm:mt-0 mb-6" src={imageUri} />
         <div className="w-full flex items-center justify-center">
           <h1 className="text-sm w-5/6 text-black text-left">{name}</h1>
         </div>
+        </div>
+        <div className="flex grow h-full justify-center items-end pb-2 mt-4 sm:mt-0">
         <a
-          className="w-32 rounded-full no-underline bg-linear-gradient h-14 border-[#FFD065] flex justify-center items-center text-black font-mono"
+          className="shadow-lg w-28 rounded-full no-underline bg-linear-gradient h-12 border-[#FFD065] flex justify-center items-center text-black font-mono"
           href={`https://www.prisjakt.nu/produkt.php?p=${id}`}
           target="_blank"
         >
           Köp
         </a>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ export default {
       backgroundImage: {
         'radial-gradient-second': 'radial-gradient(82.96% 82.96% at 50% 59.12%, rgba(132, 205, 228, 0.30) 19.79%, #84CDE4 100%)',
         'radial-gradient-first': 'radial-gradient(118.23% 118.23% at 50% 23.84%, #84CDE4 0%, rgba(132, 205, 228, 0.27) 51.09%)',
-        'linear-gradient': 'linear-gradient(180deg, rgba(132, 205, 228, 0.00) 0%, rgba(132, 205, 228, 0.14) 84.43%)',
+        'linear-gradient': 'linear-gradient(180deg, rgba(134, 166, 177, 0.14) 10.78%, rgba(27, 123, 159, 0.14) 69.1%)',
         'box-shadow': {
           'second': 'box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25)'
         }
